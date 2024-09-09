ac39b18e87048571eb2b8e87c17320eedbbc7ee0
Seth Addo <addoseth35@gmail.com>
2024-09-09 22:38:05 +0000

    feat: add layouts for project

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next";
+import { ReactNode } from "react";
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+
+export const metadata: Metadata = {
+  title: "Meet Eth",
+  description:
+    "Meet Eth is a platform for connecting with people (me - Seth Addo) and sharing ideas.",
+};
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main className="relative">
+      <Navbar />
+
+      <div className="flex">
+        <Sidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+          <div className="w-full">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default RootLayout;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+import StreamVideoProvider from '@/providers/stream-client-provider';
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
